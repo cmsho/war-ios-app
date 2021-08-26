a43e08211a89ffdf9e3f0511eb96166d918af3cf
cmsho <55258597+cmsho@users.noreply.github.com>
2021-08-25 20:05:36 -0400

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Colton Shoenberger on 8/25/21.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
